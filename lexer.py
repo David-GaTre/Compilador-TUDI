c13f849fe7ceec7a43225005d6a05c0ac9d7bc0b
David Garcia <gtdavid2013@gmail.com>
2022-10-03 15:57:39 -0500

    Added tokens to the lexer

--- lexer.py
+++ lexer.py
@@ -2,34 +2,57 @@ import ply.lex as lex
 import ply.yacc as yacc
 
 reserved = {
-   'if' : 'IF',
-   'else' : 'ELSE',
-   'game' : 'GAME',
-   'start' : 'START',
-   'float' : 'FLOAT',
-   'int' : 'INT',
-   'for': 'FOR',
-   'while': 'WHILE'
+    'start' : 'START',
+    'update': 'UPDATE',
+    'game': 'GAME',
+    'int': 'INT',
+    'float' : 'FLOAT',
+    'boolean': 'BOOLEAN',
+    'char': 'CHAR',
+    'void': 'VOID',
+    'canvas': 'CANVAS',
+    'sprite': 'SPRITE',
+    'func': 'FUNC',
+    'return': 'RETURN',
+    'declare': 'DECLARE',
+    'if': 'IF',
+    'else': 'ELSE',
+    'for': 'FOR',
+    'while': 'WHILE'
 }
 
-tokens = ['SEMICOLON','COLON', 'COMMA', 'EQUALS', 'LCURLYB', 'RCURLYB', 
-    'LPAREN','RPAREN', 'PLUS', 'MINUS', 'DIVISION', 'TIMES',
-    'CTE_I', 'CTE_F', 'CTE_STRING', 'ID', 'RELOP'] + list(reserved.values())
+tokens = ['SEMICOLON', 'COLON', 'COMMA', 'ASSIGN', 'LCURLYB', 'RCURLYB', 'LPAREN',
+    'RPAREN', 'LBRACKET', 'RBRACKET', 'PLUS', 'MINUS', 'DIVISION', 'TIMES','GTRTHAN', 
+    'GTREQLTHAN', 'LESSTHAN', 'LESSEQLTHAN', 'EQUALS', 'NOTEQUALS','AND', 'OR', 'NOT',
+    'INT_LITERAL', 'FLOAT_LITERAL', 'BOOL_LITERAL', 'STRING_LITERAL', 'ID'] + list(reserved.values())
 
 t_SEMICOLON = r'\;'
+t_COLON = r'\:'
 t_COMMA = r'\,'
-t_EQUALS = r'\='
+t_ASSIGN = r'\='
 t_LCURLYB = r'\{'
 t_RCURLYB = r'\}'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
 t_PLUS = r'\+'
 t_MINUS = r'\-'
 t_DIVISION = r'\/'
 t_TIMES = r'\*'
-t_CTE_I = r'[0-9]+'
-t_CTE_F = r'[0-9]+(\. [0-9]+)'
-t_CTE_STRING = r'\"(\w+|\s)+\"' # for word and space chars
+t_GTRTHAN = r'\>'
+t_GTREQLTHAN = r'\>\='
+t_LESSTHAN = r'\<'
+t_LESSEQLTHAN = r'\<\='
+t_EQUALS = r'\=\='
+t_NOTEQUALS = r'\!\='
+t_AND = r'\y'
+t_OR = r'\o'
+t_NOT = r'\n\o'
+t_INT_LITERAL = r'[0-9]+'
+t_FLOAT_LITERAL = r'[0-9]+(\. [0-9]+)'
+t_BOOL_LITERAL = r'(true) | (false)'
+t_STRING_LITERAL = r'\"(\w+|\s)+\"' # for word and space chars
 t_ignore  = ' \t'
 
 def t_ID(t):
